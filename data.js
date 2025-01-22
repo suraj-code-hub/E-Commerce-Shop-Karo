a775e27ba6329b7b6314aae93ae3c58c734d5d66
Suraj Kumar <160877984+suraj-code-hub@users.noreply.github.com>
2025-01-22 12:43:20 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,87 @@
+let shopItemsData = [
+    {
+    "id": 1,
+    "name": "Twisted wire",
+    "price": 20.95,
+    "desc": "Red-Yellow",
+    "img": "q4.jpg",
+    
+}, {
+    "id": 2,
+    "name": "Polycab Wire ",
+    "price": 45,
+    "desc": "Green Wire",
+    "img": "e4.jpg",
+   
+}, {
+    "id": 3,
+    "name": "Fan",
+    "price": 1850,
+    "desc": "Remot-Control",
+    "img": "e6.jpg"
+   
+}, {
+    "id": 4,
+    "name": "V Guard Fan",
+    "price": 1500,
+    "desc": "Wall Fan",
+    "img": "e7.jpg"
+    
+}, {
+    "id": 5,
+    "name": "Led Bulb ",
+    "price": 1000,
+    "desc": "4 in 1 Pack",
+    "img": "e8.jpg"
+    
+}, {
+    "id": 6,
+    "name": "Halonix Bulb",
+    "price": 999,
+    "desc": "Bulb With Speaker",
+    "img": "e9.jpg"
+    
+}, {
+    "id": 7,
+    "name": "Syska Led Bulb",
+    "price": 600,
+    "desc": "9W",
+    "img": "e10.jpg"
+  
+}, {
+    "id": 8,
+    "name": "White Led Bulb",
+    "price": 1500,
+    "desc": "20 in 1 Box",
+    "img": "e11.jpg"
+    
+}, {
+    "id": 13,
+    "name": "Fan",
+    "price": 2000,
+    "desc": "Remotable fan",
+    "img": "e16.jpg"
+    
+}, {
+    "id": 14,
+    "name": "Small Fan",
+    "price": 1200,
+    "desc": "Four Blade",
+    "img": "e17.jpg"
+  
+}, {
+    "id": 15,
+    "name": "Wall Fan",
+    "price": 1500,
+    "desc": "Three blade",
+    "img": "e18.jpg"
+   
+}, {
+    "id": 16,
+    "name": "Stand fan",
+    "price": 1200,
+    "desc": "Three blade",
+    "img": "e19.jpg",
+   
+}
+]
